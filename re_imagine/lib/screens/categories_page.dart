ddda1b9be98c806bec82b3078fa5f71cd5a7e3cd
Tanmay Tete <tanmay.tete@somaiya.edu>
2021-09-25 15:34:42 +0530

    categories beginning

---
+++ re_imagine/lib/screens/categories_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:re_imagine/components/bottom_navigation_bar_components.dart';
+import '../constants.dart';
+
+class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: height * 0.01,
+            ),
+            Center(
+              child: Text(
+                'Cartegories',
+                style: GoogleFonts.ubuntu(
+                  color: kHeadingColor,
+                  fontSize: 35,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  width: width / 2,
+                  height: height / 5,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/assassin.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Hello",
+                      style: GoogleFonts.ubuntu(color: Colors.white),
+                    ),
+                  ),
+                  margin: EdgeInsets.all(8),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBarComponent(2),
+    );
+  }
+}
