ecba1283eed784ab51d482032612e4b2261232bc
Tanmay Tete <tanmay.tete@somaiya.edu>
2021-09-26 16:36:33 +0530

    Create Sub-categories page,Modified resposiveness need to fix:sub categories result page, device size wala, navigation ka kuch

--- re_imagine/lib/main.dart
+++ re_imagine/lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:re_imagine/screens/sub_category_page.dart';
 import 'screens/search_page.dart';
 import 'screens/show_posts.dart';
 import 'screens/login_page.dart';
 import 'screens/home_page.dart';
 import 'screens/categories_page.dart';
+import 'screens/sub_category_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static DeviceSize getDeviceSize(BuildContext context) {
+    return DeviceSize(
+        MediaQuery.of(context).size.height, MediaQuery.of(context).size.width);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Reimagine",
-      // home: LoginPage(),
-      // home: HomePage(),
-      // home: SearchPage(),
-      home: CategoriesPage(),
-      // home: ShowPosts(),
+    return MediaQuery(
+      data: new MediaQueryData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Reimagine",
+        // home: LoginPage(),
+        // home: HomePage(),
+        // home: SearchPage(),
+        // home: CategoriesPage(),
+        home: SubCategoryPage(),
+        // home: ShowPosts(),
+      ),
     );
+  }
+}
+
+class DeviceSize {
+  double height;
+  double width;
+  DeviceSize(this.height, this.width);
+  static DeviceSize getDeviceSize(BuildContext context) {
+    return DeviceSize(
+        MediaQuery.of(context).size.height, MediaQuery.of(context).size.width);
   }
 }
---
+++ re_imagine/lib/screens/sub_category_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:re_imagine/components/bottom_navigation_bar_components.dart';
+import 'package:re_imagine/main.dart';
+import '../constants.dart';
+import '../main.dart';
+
+class SubCategoryPage extends StatelessWidget {
+  SubCategoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // double width = MediaQuery.of(context).size.width;
+    // double height = MediaQuery.of(context).size.height;
+    DeviceSize size = DeviceSize.getDeviceSize(context);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: kBackgroundColor,
+        body: Column(
+          children: [
+            // ListTile(
+            //     horizontalTitleGap: 0,
+
+            //     contentPadding: EdgeInsets.symmetric(horizontal: 0),
+            //     leading: IconButton(
+            //         onPressed: () {
+            //           Navigator.pop(context);
+            //         },
+            //         icon: Icon(Icons.arrow_back_ios_new_sharp)),
+            //     title: Center(
+            //       child: Text(
+            //         'Here',
+            //         style:
+            //             GoogleFonts.ubuntu(fontSize: 20, color: kHeadingColor),
+            //       ),
+            //     )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.arrow_back_ios_new_sharp,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: size.width * 0.35,
+                      vertical: size.height * 0.03),
+                  child: Text(
+                    'Here',
+                    style:
+                        GoogleFonts.ubuntu(fontSize: 30, color: kHeadingColor),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 2,
+              width: size.width * 0.9,
+              color: Colors.black,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(8),
+                  itemCount: 5,
+                  // children:
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(14),
+                          // margin: EdgeInsets.all(10),
+                          child: Text(
+                            'data',
+                            style: GoogleFonts.ubuntu(
+                              fontSize: 20,
+                              color: kHeadingColor,
+                            ),
+                          ),
+                        ),
+                        // Container(
+                        //   height: 2,
+                        //   width: size.width * 0.8,
+                        //   color: Colors.black,
+                        // )
+                      ],
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
