d3ac90a0395c688821b7f151e6a7c7bc0ee31d1f
Tanmay Tete <tanmay.tete@somaiya.edu>
2021-10-14 10:48:52 +0530

    removed back button from profile page

--- re_imagine/lib/main.dart
+++ re_imagine/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: "Reimagine",
         // home: LoginPage(),
-        // home: HomePage(),
+        home: HomePage(),
         // home: PostPage(),
         // home: SearchPage(),
         // home: CategoriesPage(),
